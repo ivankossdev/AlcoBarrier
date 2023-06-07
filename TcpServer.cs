24f4bea021c12de873655670cb019364cf0c48e4
ivankossdev <ivan.koss@yandex.ru>
2023-06-07 16:31:53 +0300

    TcpListener

--- TcpServer.cs
+++ TcpServer.cs
@@ -1,12 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace AlcoBarrier
 {
-    internal class TcpServer
+    static class TcpServer
     {
+        public static void Server()
+        {
+            TcpListener server = null;
+            try
+            {
+                
+                Int32 port = 10500;
+                IPAddress localAddr = IPAddress.Parse("192.168.0.204");
+                
+                server = new TcpListener(localAddr, port);
+                server.Start();
+
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    var client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        data = Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+                    }
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                server.Stop();
+            }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
     }
 }
