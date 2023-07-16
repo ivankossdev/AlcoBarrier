48e4705d8e32223e968fc9a186bd0d8074b2a104
ivankossdev <ivan.koss@yandex.ru>
2023-07-16 17:31:00 +0300

    !!! writedb func !!!

--- SqLiteHandler.cs
+++ SqLiteHandler.cs
@@ -16,8 +16,6 @@ namespace AlcoBarrier
             File.Delete("employees.db");
             using (var connection = new SqliteConnection("Data Source=employees.db"))
             {
-                // Name-Карта_1 User ID-U2 Card code-37358 hex 1A00000025000000EE910000 id-4ec8ed35-8e89-4bc9-b1d2-856c440cf969
-
                 connection.Open();
                 var command = connection.CreateCommand();
                 command.CommandText =
@@ -33,11 +31,23 @@ namespace AlcoBarrier
                 ";
                 command.ExecuteNonQuery();
                 connection.Close();
+
+                WriteDB("Карта_1", "U2", "37358", "1A00000025000000EE910000", "4ec8ed35-8e89-4bc9-b1d2-856c440cf969");
             }
         }
-        public static void WriteDB()
+        public static void WriteDB(string name, string iduser, string code, string hex, string id)
         {
-            
+            using (var connection = new SqliteConnection("Data Source=employees.db"))
+            {
+                // Name-Карта_1 User ID-U2 Card code-37358 hex 1A00000025000000EE910000 id-4ec8ed35-8e89-4bc9-b1d2-856c440cf969
+
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = $" INSERT INTO user (name, iduser, code, hex, id) VALUES (\"{name}\", \"{iduser}\", \"{code}\", \"{hex}\", \"{id}\")";
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+
         }
     }
 }
