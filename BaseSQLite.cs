af13aa59d718dab425b76c6d64a7bd69f0732e3b
ivankossdev <ivan.koss@yandex.ru>
2023-07-20 08:39:45 +0300

    Base Sql class

---
+++ BaseSQLite.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlcoBarrier
+{
+    public class BaseSQLite
+    {
+        readonly string db = string.Empty;
+        public BaseSQLite(string NameDataBase)
+        {
+            db = NameDataBase;
+        }
+
+        protected void Write(string sqlCommand)
+        {
+            using (var connection = new SqliteConnection($"Data Source={db}.db"))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = sqlCommand;
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        protected string[] Read(string sqlCommand) 
+        {
+            string[] result = null;
+            using (var connection = new SqliteConnection($"Data Source={db}.db"))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = sqlCommand;
+
+                using (var reader = command.ExecuteReader())
+                {
+                    int lenghtArray = reader.FieldCount;
+                    string[] Data = new string[lenghtArray];
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < lenghtArray; i++)
+                        {
+                            Data[i] = reader.GetString(i);
+                        }
+                    }
+                    result = Data;
+                }
+                connection.Close();
+            }
+
+            return result;
+        }
+    }
+}
