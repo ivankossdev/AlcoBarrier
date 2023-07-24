9ea8c11b791443c6b49bec0c8bbe58cdae68e71c
ivankossdev <ivan.koss@yandex.ru>
2023-07-24 20:01:48 +0300

    ref create settings db

--- EditDataBases.cs
+++ EditDataBases.cs
@@ -13,21 +13,9 @@ namespace AlcoBarrier
 {
     public partial class EditDataBases : Form
     {
-        SettingsDB SetDb = new SettingsDB("Settings");
         public EditDataBases()
         {
             InitializeComponent();
-            
-            string CurrentDb = Directory.GetCurrentDirectory() + "\\Settings.db";
-            if (!File.Exists(CurrentDb))
-            {
-
-                buttonCreateSettings.Enabled = true;
-            }
-            else
-            {
-                buttonCreateSettings.Enabled = false;
-            }
         }
 
         private void buttonSyncBDInnerage_Click(object sender, EventArgs e)
@@ -37,14 +25,34 @@ namespace AlcoBarrier
 
         private void buttonCreateSettings_Click(object sender, EventArgs e)
         {
-            string message = SetDb.CreateDB();
+            string message = CreateDbForButton(new SettingsDB("Settings"));
             textBoxLog.AppendText($"{message} \n");
-            buttonCreateSettings.Enabled = false;
         }
 
         private void buttonCreateEventsDB_Click(object sender, EventArgs e)
         {
             textBoxLog.AppendText("БД События\n");
+        }
+
+        private string CreateDbForButton(object obj)
+        {
+
+            string message = string.Empty;
+            if (obj is SettingsDB setDb)
+            {
+                string CurrentDb = Directory.GetCurrentDirectory() + "\\Settings.db";
+                if (!File.Exists(CurrentDb))
+                {
+                    message = setDb.CreateDB();
+                }
+                else
+                {
+                    message = "База данных настроек уже создана.";
+                }
+            }
+
+
+            return message;
         }
     }
 }
